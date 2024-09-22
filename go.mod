1f659bccba87089b2af84b2562dd7685ae4a3328
xpanvictor <xpanvictor@gmail.com>
2024-09-22 15:49:53 +0100

    feat(recipe): create new recipe

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module recipes-api
 
 go 1.23.1
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/rs/xid v1.6.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+	"net/http"
 	"time"
 )
 
@@ -15,14 +17,30 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
-func createRecipes(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello world",
-	})
+// General store
+var recipes []Recipe
+
+func initDB() {
+	recipes = make([]Recipe, 0)
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	// marshal the request body into a Recipe struct
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(400, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusCreated, recipe)
 }
 
 func main() {
 	router := gin.Default()
-	router.POST("/", createRecipes)
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run(":5000")
 }
