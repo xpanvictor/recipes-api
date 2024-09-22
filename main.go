265793a71d9c12e999511a9f54b6fa3c6ac7ff8e
xpanvictor <xpanvictor@gmail.com>
2024-09-22 10:02:01 +0100

    feat(model): recipe model

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+// Recipe Define struct for recipe
+type Recipe struct {
+	ID           string    `json:"ID"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Instructions []string  `json:"instructions"`
+	Ingredients  []string  `json:"ingredients"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+func createRecipes(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello world",
+	})
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/", createRecipes)
+	router.Run(":5000")
+}
